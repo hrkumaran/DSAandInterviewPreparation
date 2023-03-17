4a1bc792ab55424df2b5556aec901db3542f0c2f
Kumaran <kumaranbecse@gmail.com>
2023-03-17 08:03:26 +0530

    Remove the duplicate object using streams

--- src/com/google/tryit/ds/interview/Test.java
+++ src/com/google/tryit/ds/interview/Test.java
@@ -1,7 +1,9 @@
 package com.google.tryit.ds.interview;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Test {
@@ -10,13 +12,13 @@ public class Test {
         Employee e = new Employee(1,"ravi","coimbatore");
         Employee e1 = new Employee(1,"ravi","coimbatore");
         Employee e2 = new Employee(1,"ravi","coimbatore");
-        List<Employee> employeeList = new ArrayList<Employee>();
+        Set<Employee> employeeList = new HashSet<Employee>();
 
         employeeList.add(e);
         employeeList.add(e1);
         employeeList.add(e2);
         employeeList.stream().forEach((c) -> System.out.println("employeeList->"+c.age+":"+c.address+":"+c.name));
-        List<Employee> ditinctemployeeList =employeeList.stream().distinct().collect(Collectors.toList());
+        Set<Employee> ditinctemployeeList =employeeList.stream().distinct().collect(Collectors.toSet());
         ditinctemployeeList.stream().forEach((c) -> System.out.println("ditinctemployeeList->"+c.age+":"+c.address+":"+c.name));
     }
 }
