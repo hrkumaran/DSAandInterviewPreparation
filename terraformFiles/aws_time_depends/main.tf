1f4ad15ed0a2b2148b8c607993bb668f7de919f8
Kumaran <kumaranbecse@gmail.com>
2023-04-05 15:03:57 +0530

    Terraform Training Material Day 3 time sleep

---
+++ terraformFiles/aws_time_depends/main.tf
@@ -0,0 +1,10 @@
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [null_resource.previous]
+  create_duration = "30s"
+}
+
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_30_seconds]
+}
+
+resource "null_resource" "previous" {}
